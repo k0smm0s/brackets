e706062b56fc68ed7e0af1fa3766c6ee1e52ae3e
Dmitry Serediuk <k0sm0s@tut.by>
2019-03-13 20:12:01 +0300

    feat: all tests pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+/**
+ * Merge open and close brackets
+ * @param {Array} bracketsConfig Array of brackets open and close
+ * @returns {Array} arrBracketsStr Array of string open + close
+ */
+function formBracketsArr(bracketsConfig){
+  var arrBracketsStr = [];
+  var bracketsStr = "";
+  for (var i=0; i < bracketsConfig.length; i++){
+    bracketsStr = "";
+    for (var j=0; j < bracketsConfig[i].length; j++){
+      bracketsStr += bracketsConfig[i][j];
+    }
+    arrBracketsStr.push(bracketsStr);
+  }
+  return arrBracketsStr;
+}
+
+/**
+ * Main function use str.replace for delete correctly brackets
+ */
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var bracketsArr = formBracketsArr(bracketsConfig);
+  while(str != ""){
+    strOld = str;
+    for (var i=0; i<bracketsArr.length; i++){
+      str = str.replace(bracketsArr[i],"");
+    }
+    if (strOld == str)
+      return false;
+  }
+  return true;
+
 }
